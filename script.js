a3789363055f980b04824186d989c9a68e6e0cbb
Siva rama raju Tirumalaraju <113761483+20a31a04u4@users.noreply.github.com>
2023-11-13 16:14:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// You can add JavaScript functionality here if needed
+document.querySelector('.healthbot-heading').addEventListener('click', function() {
+    alert('HealthBot clicked!');
+});
